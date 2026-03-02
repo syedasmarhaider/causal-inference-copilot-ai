01c9f41f0815cc3fc7d7bce6cd5b121e56649de7
SyedAsmar <89920359+SyedAsmar@users.noreply.github.com>
2026-03-02 04:08:51 +0100

    freez

--- src/python/implementation/workflows/tools/tools_factory.py
+++ src/python/implementation/workflows/tools/tools_factory.py
@@ -7,6 +7,7 @@ from python.domain.workflows.tool import Tool
 from python.domain.workflows.tool_factory import ToolFactory
 from python.implementation.workflows.tools.causal.causal_model_factory_tool import CausalModelFactoryTool
 from python.implementation.workflows.tools.data_processing.data_processing_tool import DataProcessingTool
+from python.implementation.workflows.tools.data_profiling.causal_data_profiling_tool import CausalDataProfilingTool
 from python.implementation.workflows.tools.data_profiling.data_profiling_tool import DatasetProfilingTool
 from python.implementation.workflows.tools.encoding.encoding_tool import EncodingTool
 
@@ -18,6 +19,7 @@ class DefaultToolFactory(ToolFactory):
             DatasetProfilingTool.NAME: DatasetProfilingTool(),
             EncodingTool.NAME: EncodingTool(),
             DataProcessingTool.NAME: DataProcessingTool(),
+            CausalDataProfilingTool.NAME: CausalDataProfilingTool(),
             CausalModelFactoryTool.NAME: CausalModelFactoryTool.create_default(data_repo=data_repo, models_repo=models_repo),
         }
 
