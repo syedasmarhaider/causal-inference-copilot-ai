0834fdffd8421d2cd0e1e63c4123d3abfefd7ea9
SyedAsmar <89920359+SyedAsmar@users.noreply.github.com>
2026-04-21 14:29:23 +0200

    fixes

--- src/python/implementation/workflows/nodes/model_selection/model_selection_node.py
+++ src/python/implementation/workflows/nodes/model_selection/model_selection_node.py
@@ -373,7 +373,7 @@ class ModelSelectionNode(Node):
                 "assistant_message": (
                     f"Confirmed model selection: {selected_recommendation.display_label}. "
                     f"{decision.assistant_message}"
-                    f" Training will take some time so be with me I will let you know once it\'s done.",
+                    "Training will take some time so meanwhile you can have some rest"
                 ),
                 "system_message": None,
                 "error_message": None,
